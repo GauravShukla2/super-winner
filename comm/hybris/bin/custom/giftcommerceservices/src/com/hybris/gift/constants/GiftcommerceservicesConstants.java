9900afd79574f8143317902e29d50b4c4e3827c4
Gaurav Shukla <gshukla2@sapient.com>
2016-02-22 17:11:03 +0530

    Tweaking SolR Attributes and Facets

---
+++ comm/hybris/bin/custom/giftcommerceservices/src/com/hybris/gift/constants/GiftcommerceservicesConstants.java
@@ -0,0 +1,29 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2013 hybris AG
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of hybris
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with hybris.
+ * 
+ *  
+ */
+package com.hybris.gift.constants;
+
+/**
+ * Global class for all Giftcommerceservices constants. You can add global constants for your extension into this class.
+ */
+public final class GiftcommerceservicesConstants extends GeneratedGiftcommerceservicesConstants
+{
+	public static final String EXTENSIONNAME = "giftcommerceservices";
+
+	private GiftcommerceservicesConstants()
+	{
+		//empty to avoid instantiating this constant class
+	}
+
+	// implement here constants used by this extension
+}
