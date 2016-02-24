ae64db7377198e33c209842704d59fa9a998f7e3
Gaurav Shukla <gshukla2@sapient.com>
2016-02-24 11:44:44 +0530

    Personalizing Content of the Shop-4

--- comm/hybris/bin/custom/gift/giftcore/src/com/hybris/gift/core/btg/condition/operand/valueprovider/CustomerInternalFlagValueProvider.java
+++ comm/hybris/bin/custom/gift/giftcore/src/com/hybris/gift/core/btg/condition/operand/valueprovider/CustomerInternalFlagValueProvider.java
@@ -1,36 +1,37 @@
 package com.hybris.gift.core.btg.condition.operand.valueprovider;
- 
-import com.hybris.gift.core.model.BTGCustomerInternalFlagOperandModel;
+
 import de.hybris.platform.btg.condition.operand.OperandValueProvider;
 import de.hybris.platform.btg.enums.BTGConditionEvaluationScope;
 import de.hybris.platform.btg.jalo.BTGException;
 import de.hybris.platform.core.model.user.CustomerModel;
 import de.hybris.platform.core.model.user.UserModel;
- 
- 
+
+import com.hybris.gift.core.model.BTGCustomerInternalFlagOperandModel;
+
+
 /**
- * @author Gaurav
+ * @author KKW
  *
  */
-public abstract class CustomerInternalFlagValueProvider implements OperandValueProvider<BTGCustomerInternalFlagOperandModel>
+public class CustomerInternalFlagValueProvider implements OperandValueProvider<BTGCustomerInternalFlagOperandModel>
 {
- 
-    @Override
-    public Object getValue(final BTGCustomerInternalFlagOperandModel operand, final UserModel user,
-            final BTGConditionEvaluationScope scope)
-    {
-        if (user instanceof CustomerModel)
-        {
-            return ((CustomerModel) user).getIsInternal();
-        }
-        throw new BTGException("user must be of type [Customer]");
- 
-    }
- 
-    @Override
-    public Class getValueType(final BTGCustomerInternalFlagOperandModel arg0)
-    {
-        return Boolean.class;
-    }
- 
+
+	@Override
+	public Object getValue(final BTGCustomerInternalFlagOperandModel operand, final UserModel user,
+			final BTGConditionEvaluationScope scope)
+	{
+		if (user instanceof CustomerModel)
+		{
+			return ((CustomerModel) user).getIsInternal();
+		}
+		throw new BTGException("user must be of type [Customer]");
+
+	}
+
+	@Override
+	public Class getValueType(final BTGCustomerInternalFlagOperandModel arg0)
+	{
+		return Boolean.class;
+	}
+
 }
